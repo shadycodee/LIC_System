e2390e0ac7dbfa794502fa3324c98cb945328a80
shadycodee <mikojohn158@gmail.com>
2023-11-07 13:27:02 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'records'
+
+mysql = MySQL(app)
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
+
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['pass']
+
+        print("Entered Username: ", username)
+        print("Entered Password: ", password)
+
+        
+
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            return redirect(url_for('manage'))
+        else:
+            return render_template('admin_login.html', message='Invalid Credentials')
+    return render_template('admin_login.html')
+
+@app.route('/manage', methods=['GET', 'POST'])
+def manage():
+    if request.method == 'POST':
+        student_id = request.form['studentid']
+        name = request.form['name']
+        password = request.form['password']
+        course = request.form['course']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (student_id, name, password, course) VALUES (%s, %s, %s, %s)",
+                    (student_id, name, password, course))
+        mysql.connection.commit()
+        cur.close()
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT student_id, name, course FROM students")
+    students = cur.fetchall()
+    cur.close()
+
+    return render_template('manage.html', students=students)
+
+@app.route('/login')
+def student_login():
+    return render_template('student_login.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
